d75838604ca41e1315e380536b0687f5d60af514
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-12-01 14:19:02 +0300

    Completed Assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+    def grades
+        students = Student.all
+        # render json: students from lowest to highest
+        render json: students.order(grade: :desc)
+    end
+    def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
+    end
+end
